b23c1c3fc6f223cea39b2ce536793001cd40309c
John Yevsukov <github.com@john.engineering>
2021-06-24 03:01:20 -0400

    testing fe/be connection

--- src/App.js
+++ src/App.js
@@ -1,7 +1,21 @@
 import './App.css';
+import React, { useState, useEffect } from 'react'
 import { Route, Switch } from 'react-router-dom'
+import axios from 'axios'
 
 function App() {
+  const [pets, setPets] = useState([])
+
+  useEffect(() => {
+    axios.get('https://pet-post.herokuapp.com/api/users')
+    .then(users => {
+      setPets(users.data)
+    })
+    .catch(err => {
+      console.log(err)
+    })
+  }, [])
+
   return (
     <div className="App">
       <h1>🐹🦎 Pet Post 🐱🐶</h1>
@@ -11,6 +25,17 @@ function App() {
         </Route>
         <Route path='/animals'>
           <h1>Animals!</h1>
+          {
+            pets.map(pet => {
+              return(
+              <div>
+                <h2>{pet.user_username}</h2>
+                <h3>{pet.user_species}</h3>
+                <p>{pet.user_email}</p>
+              </div>
+              )
+            })
+          }
         </Route>
       </Switch>
     </div>
